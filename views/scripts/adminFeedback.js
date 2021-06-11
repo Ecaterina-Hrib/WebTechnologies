2f1ca685da5c5fed6dbe3cf1e1842383288bede3
Ecaterina Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-06-12 01:28:35 +0300

    integrate back feedback

---
+++ views/scripts/adminFeedback.js
@@ -0,0 +1,35 @@
+function on(el) {
+    let div = el.parentElement.children[1];
+    div.style.display = "block";
+    div.style.width = "100%";
+}
+function off(el) {
+    el.style.display = "none";
+    el.style.width = "0";
+}
+
+function buildFeedback(firstName,lastName,subject){
+    let s = `         <div class="feedback">
+    <p class="date"> <b> ${firstName}  ${lastName}</b> </p>
+    <p class="textul">${subject} </p>
+</div>`
+return s;
+}
+function showFeedbacks(feedbacks){
+    window.feedbacks=feedbacks;
+    document.getElementById("productSectionAdmin").innerHTML = "";
+    for (let element of feedbacks) {
+        var feedback = document.createElement("div");
+        feedback.innerHTML = buildFeedback(element["firstName"], element["lastName"], element["subject"])
+        document.getElementById("productSectionAdmin").appendChild(feedback);
+    }
+}
+async function populateDataFeedback() {
+
+    const response = (await fetch("http://localhost:3000/api/v1/feedbacks/", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })).json().then((data) => showFeedbacks(data["feedbacks"]));
+}
