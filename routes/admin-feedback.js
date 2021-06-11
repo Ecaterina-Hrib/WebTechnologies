ef76f07a02ae67f191155dd9cb344255482b0564
Ecaterina Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-06-12 01:29:27 +0300

    integrate back feedback

--- routes/admin-feedback.js
+++ routes/admin-feedback.js
@@ -9,6 +9,7 @@ router.get('/admin-feedback.html', adminFeedbackController.getAdminFeedbackHTML)
 router.get('/styles/nav.css', adminFeedbackController.getAdminFeedbackCSS2)
 router.get('/styles/admin-feedback.css',adminFeedbackController.getAdminFeedbackCSS1)
 router.get('/scripts/nav.js', adminFeedbackController.getAdminFeedbackJS)
+router.get('/scripts/adminFeedback.js', adminFeedbackController.getAdminFeedbackJS2)
 router.get('/feedback',adminFeedbackController.getProducts)
 
 module.exports.adminFeedback = router
